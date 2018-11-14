72fc9344cebc43345166458308ef741575d415d1
Tamer Alnoami <tamernoami@gmail.com>
2018-11-14 23:28:51 +0100

    Added menu options and a temp button to move between activities fixes #1 fixes #2

--- app/src/main/java/android/example/com/investify/SecondActivity.java
+++ app/src/main/java/android/example/com/investify/SecondActivity.java
@@ -1,7 +1,14 @@
 package android.example.com.investify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +16,40 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+
+
+    //To activate the menu on this activity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+
+    // A temporary method to move to next activity
+    public void moveNext(View view) {
+        startActivity(new Intent(this,ThirdActivity.class));
+    }
+
+
+    // A temporary method for the selected item from the menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.about_us_id:
+                Toast.makeText(this,"About Us ",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.contact_us_id:
+                Toast.makeText(this,"Contact Us ",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.search_id:
+                Toast.makeText(this,"Search ",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
