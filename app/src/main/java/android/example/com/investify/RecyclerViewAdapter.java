13d0f3c5c40734a1884964faccf78a3c291514ce
adam2adam <adams.swd@gmail.com>
2018-11-23 12:16:10 +0100

    Second Screen Search and Filter

--- app/src/main/java/android/example/com/investify/RecyclerViewAdapter.java
+++ app/src/main/java/android/example/com/investify/RecyclerViewAdapter.java
@@ -3,18 +3,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Filter;
-import android.widget.Filterable;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import com.bumptech.glide.Glide;
 import java.util.ArrayList;
+import java.util.List;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 /**This class is responsible for preparing the items of companies list to
@@ -25,12 +24,16 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>  {
-    private ArrayList<Company> companiesList= new ArrayList<Company>();
+    private ArrayList<Company> companyList = new ArrayList<Company>();
     private Context context;
+    private List<Company> companyListFiltered;
+    private CompanyAdapterListener listener;
 
     public RecyclerViewAdapter( Context context,ArrayList<Company> companiesList) {
-        this.companiesList = companiesList;
         this.context = context;
+        //this.listener = listener;
+        this.companyList = companiesList;
+        this.companyListFiltered = companiesList;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
@@ -44,6 +47,13 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             tvName= itemView.findViewById(R.id.tvCompanyName);
             parent=itemView.findViewById(R.id.parent_layout);
 
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // send selected contact in callback
+                    listener.onCompanySelected(companyListFiltered.get(getAdapterPosition()));
+                }
+            });
         }
     }
 
@@ -57,7 +67,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
 
     public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
-        final Company company = companiesList.get(position);
+        //final Company company = companyList.get(position);
+        final Company company = companyListFiltered.get(position);
         Glide.with(context).asBitmap().load(company.getLogoLInk()).into(holder.image);
         holder.tvName.setText(company.getName());
         holder.parent.setOnClickListener(new View.OnClickListener() {
@@ -73,13 +84,52 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public int getItemCount() {
-        return companiesList.size();
+        //return companyList.size();
+        return companyListFiltered.size();
     }
 
 
-    /*public Filter getFilter() {
-        return null;
-    }*/
+    //@Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    companyListFiltered = companyList;
+                } else {
+                    List<Company> filteredList = new ArrayList<>();
+                    for (Company row : companyList) {
 
+                        // name match condition. this might differ depending on your requirement
+                        // here we are looking for name or description match
+                        //if (row.getName().toLowerCase().contains(charString.toLowerCase()) || row.getDescription().contains(charSequence)) {
 
+                        // here we are looking for only name matching
+                        if (row.getName().toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(row);
+                        }
+                    }
+
+                    companyListFiltered = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = companyListFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                companyListFiltered = (ArrayList<Company>) filterResults.values;
+
+                // refresh the list (recycler view) with filtered data
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+    public interface CompanyAdapterListener {
+        void onCompanySelected(Company company);
+    }
 }
--- app/src/main/java/android/example/com/investify/SecondActivity.java
+++ app/src/main/java/android/example/com/investify/SecondActivity.java
@@ -1,17 +1,20 @@
 package android.example.com.investify;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Filter;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -20,13 +23,14 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.apache.commons.math3.stat.regression.SimpleRegression;
-
 import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
     private static final String TAG = "SecondActivity";
     private ArrayList<Company> companiesList= new ArrayList<>();
+    private RecyclerView recyclerView;
+    RecyclerViewAdapter adapter;
+    private SearchView searchView;
    // private ArrayList<String> companyNameList= new ArrayList<>();
     //private ArrayList <String> companyLogoList= new ArrayList<>();
     FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -37,6 +41,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        //final RecyclerView recyclerView = findViewById(R.id.reviewCompanyList);
+        //final RecyclerViewAdapter adapter = new RecyclerViewAdapter(SecondActivity.this, companiesList);
+
 
         int revenue = (int) getIntent().getDoubleExtra("Revenue",0);
         int amount = (int) getIntent().getDoubleExtra("Amount",0);
@@ -55,10 +63,12 @@ public class SecondActivity extends AppCompatActivity {
                     companiesList.add(company);
                     Log.d(TAG, "Value is: " + company.toString());
                 }
-                RecyclerView recyclerView = findViewById(R.id.reviewCompanyList);
-                RecyclerViewAdapter adapter = new RecyclerViewAdapter(SecondActivity.this, companiesList);
+                recyclerView = findViewById(R.id.reviewCompanyList);
+                adapter = new RecyclerViewAdapter(SecondActivity.this, companiesList);
                 recyclerView.setAdapter(adapter);
+                //adapter.notifyDataSetChanged();
                 recyclerView.setLayoutManager(new LinearLayoutManager(SecondActivity.this));
+
             }
             @Override
             public void onCancelled(DatabaseError error) {
@@ -67,6 +77,7 @@ public class SecondActivity extends AppCompatActivity {
             }
         });
         int x = 0;
+        //adapter.notifyDataSetChanged();
 
         tvRevenue.setText("If you invest " + amount + " your estimate profit will be "+String.valueOf(revenue-amount));
 
@@ -82,6 +93,33 @@ public class SecondActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater= getMenuInflater();
         inflater.inflate(R.menu.main_menu,menu);
+        /**
+         * Associate searchable configuration with the SearchView         *
+         */
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.action_search)
+                .getActionView();
+        searchView.setSearchableInfo(searchManager
+                .getSearchableInfo(getComponentName()));
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+
+        // listening to search query text change
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // filter recycler view when query submitted
+                adapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                // filter recycler view when text is changed
+                adapter.getFilter().filter(query);
+                return false;
+            }
+        });
         return true;
     }
 
@@ -102,11 +140,21 @@ public class SecondActivity extends AppCompatActivity {
             case R.id.contact_us_id:
                 Toast.makeText(this,"Contact Us ",Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.search_id:
+            case R.id.action_search:
                 Toast.makeText(this,"Search ",Toast.LENGTH_SHORT).show();
                 break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // close search view on back button pressed
+        if (!searchView.isIconified()) {
+            searchView.setIconified(true);
+            return;
+        }
+        super.onBackPressed();
     }
 }
